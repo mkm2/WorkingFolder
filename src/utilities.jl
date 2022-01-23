bd44be28093b5cf0158f8fec110899eb4ff629a4
Maximilian Muellenbach <Maximilian.Muellenbach@googlemail.com>
2022-01-23 19:24:49 +0100

    Utilities added

---
+++ src/utilities.jl
@@ -0,0 +1,33 @@
+module Utils
+
+using SparseArrays, LinearAlgebra, Plots
+using SpinSymmetry, JLD2
+using KrylovKit
+using ..LightCones
+
+export logmsg, path_prefix, save
+
+function logmsg(msg...; doflush=false)
+    println("[",now(), "]", msg...)
+    doflush && flush(stdout)
+end
+
+function path_prefix(workspace)
+    try
+        abspath(joinpath(readchomp(`ws_find $workspace`)))
+    catch e
+        path_prefix()
+    end
+end
+
+function save(data, datapath)
+    dname = dirname(datapath)
+    if !isdir(dname)
+        logmsg("Save directory: $dname does not exist. Creating!")
+        mkpath(dname)
+    end
+    logmsg("Saving file: $path")
+    JLD2.jldsave(datapath; data) 
+end
+
+end #module
