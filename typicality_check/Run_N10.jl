d9d45e59ff5df14f0b4c6d1f723a8fe4bfc5a500
Maximilian Muellenbach <Maximilian.Muellenbach@googlemail.com>
2022-03-01 23:18:57 +0100

    Additional Runs

---
+++ typicality_check/Run_N10.jl
@@ -0,0 +1,18 @@
+#!/bin/sh
+# ########## Begin Slurm header ##########
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=01:00:00
+#SBATCH --mem=80gb
+#SBATCH --cpus-per-task=10
+#SBATCH --job-name=otoc_simulation_N14
+#SBATCH --output="otoc_simulation_N14-%j.out"
+########### End Slurm header ##########
+#=
+# load modules
+# not needed - julia installed locally
+
+# export JULIA_DEPOT_PATH=$SCRATCH
+export ON_CLUSTER=1
+exec julia --color=no --threads=20 --startup-file=no "$LCDIR/LightCones/typicality_check/shared_krylov.jl" 10 1 10 z $1
+=#
