f5857b723942f9df5724f9d40e59c61d807c99e2
Maximilian Müllenbach <maximilian.muellenbach@googlemail.com>
2022-03-01 23:26:20 +0100

    Extra Runs edited

--- typicality_check/Run_N10.jl
+++ typicality_check/Run_N10.jl
@@ -4,9 +4,9 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=01:00:00
 #SBATCH --mem=80gb
-#SBATCH --cpus-per-task=10
-#SBATCH --job-name=otoc_simulation_N14
-#SBATCH --output="otoc_simulation_N14-%j.out"
+#SBATCH --cpus-per-task=48
+#SBATCH --job-name=otoc_simulation_N10
+#SBATCH --output="otoc_simulation_N10-%j.out"
 ########### End Slurm header ##########
 #=
 # load modules
@@ -14,5 +14,5 @@
 
 # export JULIA_DEPOT_PATH=$SCRATCH
 export ON_CLUSTER=1
-exec julia --color=no --threads=20 --startup-file=no "$LCDIR/LightCones/typicality_check/shared_krylov.jl" 10 1 10 z $1
+exec julia --color=no --threads=96 --startup-file=no "$LCDIR/LightCones/typicality_check/shared_krylov.jl" 10 1 10 z $1
 =#
