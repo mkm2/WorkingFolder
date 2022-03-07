aee2d8e708716416833c2834991a653dc585bc48
Maximilian Muellenbach <Maximilian.Muellenbach@googlemail.com>
2022-03-07 17:12:39 +0100

    Organized data for typicality checks

--- slurm/shared_krylov.jl
+++ slurm/shared_krylov.jl
@@ -101,10 +101,11 @@ end
 
 if RANDOM_STATES == false
     otocs = zeros(length(trange),N,SHOTS)
-    #H_tot = Vector{SparseMatrixCSC{Float64,Int64}}([spzeros(2^N,2^N) for l in 1:SHOTS])
-    H_tot = Vector{Adjoint{Float64, ThreadedSparseMatrixCSC{Float64, Int64, SparseMatrixCSC{Float64, Int64}}}}([ThreadedSparseMatrixCSC(spzeros(2^N,2^N))' for l in 1:4])
+    H_tot = Vector{SparseMatrixCSC{Float64,Int64}}([spzeros(2^N,2^N) for l in 1:SHOTS])
+    #H_tot = Vector{Adjoint{Float64, ThreadedSparseMatrixCSC{Float64, Int64, SparseMatrixCSC{Float64, Int64}}}}([ThreadedSparseMatrixCSC(spzeros(2^N,2^N))' for l in 1:4])
     Threads.@threads for shot in 1:SHOTS
-        H_tot[shot] = ThreadedSparseMatrixCSC(H + field_term(DISORDER_PARAM,N))'
+        H_tot[shot] = H + field_term(DISORDER_PARAM,N)
+        #H_tot[shot] = ThreadedSparseMatrixCSC(H + field_term(DISORDER_PARAM,N))'
         @time otocs[:,:,shot] = otoc_spat(H_tot[shot],A,B,trange,ψ0,N,δt)
         logmsg("Completed Shot $(shot)")
     end
