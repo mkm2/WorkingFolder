045535c476f74fb4aff5366cc64fddf99852fc81
Maximilian Muellenbach <Maximilian.Muellenbach@googlemail.com>
2022-03-07 13:23:16 +0100

    Threaded Sparse Test

--- slurm/shared_krylov.jl
+++ slurm/shared_krylov.jl
@@ -15,8 +15,8 @@ Pkg.instantiate(; io=stdout)
 Pkg.status(; io=stdout)
 
 import Dates
-using SparseArrays, Random
-using SpinSymmetry
+using SparseArrays, ThreadedSparseArrays
+using SpinSymmetry, Random
 using LightCones
 
 ## Environment
@@ -76,7 +76,7 @@ logmsg("*"^10 * "Running simulation" * "*"^10)
 T = 5
 trange = 0:δt:T
 
-i = 3
+i = div(N,2)
 A = single_spin_op(σz,i,N)
 
 if OBSERVABLE == "x"
@@ -101,9 +101,10 @@ end
 
 if RANDOM_STATES == false
     otocs = zeros(length(trange),N,SHOTS)
-    H_tot = Vector{SparseMatrixCSC{Float64,Int64}}([spzeros(2^N,2^N) for l in 1:SHOTS])
+    #H_tot = Vector{SparseMatrixCSC{Float64,Int64}}([spzeros(2^N,2^N) for l in 1:SHOTS])
+    H_tot = Vector{Adjoint{Float64, ThreadedSparseMatrixCSC{Float64, Int64, SparseMatrixCSC{Float64, Int64}}}}([ThreadedSparseMatrixCSC(spzeros(2^N,2^N))' for l in 1:4])
     Threads.@threads for shot in 1:SHOTS
-        H_tot[shot] = H + field_term(DISORDER_PARAM,N)
+        H_tot[shot] = ThreadedSparseCSC(H + field_term(DISORDER_PARAM,N))'
         @time otocs[:,:,shot] = otoc_spat(H_tot[shot],A,B,trange,ψ0,N,δt)
         logmsg("Completed Shot $(shot)")
     end
@@ -113,7 +114,7 @@ else
     print("test")
     Threads.@threads for shot in 1:SHOTS
         for s in 1:N_RANDOM_STATES
-            H_tot[shot] = H + field_term(DISORDER_PARAM,N)
+            H_tot[shot] = ThreadedSparseCSC(H + field_term(DISORDER_PARAM,N))'
             @time otocs[:,:,shot,s] = otoc_spat(H_tot[shot],A,B,trange,ψs[:,s],N,δt)
             logmsg("Completed Shot $(shot), state $(s)")
         end
