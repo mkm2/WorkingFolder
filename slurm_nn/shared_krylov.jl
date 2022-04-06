2794252dc1410da7e50d7e6970811fec4462d169
mkm2 <Maximilian.Muellenbach@googlemail.com>
2022-04-06 17:50:34 +0200

    Ready for last run before maintenance

--- slurm_nn/shared_krylov.jl
+++ slurm_nn/shared_krylov.jl
@@ -75,7 +75,7 @@ logmsg("*"^10 * "Running simulation" * "*"^10)
 
 #Set up simulation parameters
 
-tmax = 4
+tmax = 1.0
 δt = 0.01
 T = 2
 trange = 0:δt:T
@@ -117,7 +117,7 @@ if RANDOM_STATES == false
         H_tot[shot] = H + field_term(DISORDER_PARAM,N)
         logmsg("Created Hamiltonian for Shot $(shot)")
         #H_tot[shot] = ThreadedSparseMatrixCSC(H + field_term(DISORDER_PARAM,N))'
-        @time otocs[:,:,shot] = otoc_spat(H_tot[shot],A,B,trange,ψ0,N,δt)
+        @time otocs[:,:,shot] = otoc_spat(H_tot[shot],A,B,trange,ψ0,N,tmax)
         logmsg("Completed Shot $(shot)")
     end
 else
@@ -127,7 +127,7 @@ else
     Threads.@threads for shot in 1:SHOTS
         for s in 1:N_RANDOM_STATES
             H_tot[shot] = ThreadedSparseMatrixCSC(H + field_term(DISORDER_PARAM,N))'
-            @time otocs[:,:,shot,s] = otoc_spat(H_tot[shot],A,B,trange,ψs[:,s],N,δt)
+            @time otocs[:,:,shot,s] = otoc_spat(H_tot[shot],A,B,trange,ψs[:,s],N,tmax)
             logmsg("Completed Shot $(shot), state $(s)")
         end
     end
