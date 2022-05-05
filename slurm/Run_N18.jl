8a895ce6d67c4e65c8596f2f475821f7bc34cf52
Maximilian Müllenbach <maximilian.muellenbach@googlemail.com>
2022-05-05 15:54:39 +0200

    Fixed slurm_nn

--- slurm/Run_N18.jl
+++ slurm/Run_N18.jl
@@ -2,11 +2,11 @@
 # ########## Begin Slurm header ##########
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=05:00:00
-#SBATCH --mem=40gb
+#SBATCH --time=80:00:00
+#SBATCH --mem=250gb
 #SBATCH --cpus-per-task=48
-#SBATCH --job-name=otoc_simulation_N15
-#SBATCH --output="otoc_simulation_N15_NN-%j.out"
+#SBATCH --job-name=otoc_simulation_N19_FD
+#SBATCH --output="otoc_simulation_N19_FD-%j.out"
 ########### End Slurm header ##########
 #=
 # load modules
@@ -14,5 +14,5 @@
 
 # export JULIA_DEPOT_PATH=$SCRATCH
 export ON_CLUSTER=1
-exec julia --color=no --threads=96 --startup-file=no "$LCDIR/LightCones/slurm_nn/shared_krylov.jl" 15 100 0 z $1 nn
+exec julia --color=no --threads=96 --startup-file=no "$LCDIR/LightCones/slurm/shared_krylov.jl" 19 20 0 z $1
 =#
