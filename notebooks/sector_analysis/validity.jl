774235f7d816fb4332cc67f6912f12d69d81fa7c
mkm2 <Maximilian.Muellenbach@googlemail.com>
2022-08-10 23:44:01 +0200

    Scaling Analysis

--- notebooks/sector_analysis/validity.jl
+++ notebooks/sector_analysis/validity.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.18.1
+# v0.19.9
 
 using Markdown
 using InteractiveUtils
@@ -227,7 +227,7 @@ PlutoUI = "~0.7.39"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.7.1"
+julia_version = "1.7.2"
 manifest_format = "2.0"
 
 [[deps.AbstractPlutoDingetjes]]
