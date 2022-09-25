6364a617cdfb1e7bde8ea6bad6593ee8a47de739
Maximilian Müllenbach <maximilian.muellenbach@googlemail.com>
2022-09-25 22:08:36 +0200

    fixes

--- averaging_procedure/Run_N15_BS_sector_h.jl
+++ averaging_procedure/Run_N15_BS_sector_h.jl
@@ -5,8 +5,8 @@
 #SBATCH --time=30:00:00
 #SBATCH --mem=350gb
 #SBATCH --cpus-per-task=48
-#SBATCH --job-name=otoc_simulation_N17_BS_sector_AVP
-#SBATCH --output="otoc_simulation_N17_BS_sector_AVP-%j.out"
+#SBATCH --job-name=otoc_simulation_N15_BS_sector_AVP_h
+#SBATCH --output="otoc_simulation_N15_BS_sector_AVP_h-%j.out"
 ########### End Slurm header ##########
 #=
 # load modules
@@ -14,5 +14,5 @@
 
 # export JULIA_DEPOT_PATH=$SCRATCH
 export ON_CLUSTER=1
-exec julia --color=no --threads=96 --startup-file=no "$LCDIR/LightCones/averaging_procedure/shared_krylov_sector.jl" 17 50 50 BS z $1
+exec julia --color=no --threads=96 --startup-file=no "$LCDIR/LightCones/averaging_procedure/shared_krylov_sector_h.jl" 11 50 50 BS z $1
 =#
--- averaging_procedure/Run_N15_BS_sector_mixed.jl
+++ averaging_procedure/Run_N15_BS_sector_mixed.jl
@@ -5,8 +5,8 @@
 #SBATCH --time=30:00:00
 #SBATCH --mem=350gb
 #SBATCH --cpus-per-task=48
-#SBATCH --job-name=otoc_simulation_N17_BS_sector_AVP
-#SBATCH --output="otoc_simulation_N17_BS_sector_AVP-%j.out"
+#SBATCH --job-name=otoc_simulation_N15_BS_sector_AVP_mixed
+#SBATCH --output="otoc_simulation_N15_BS_sector_AVP_mixed-%j.out"
 ########### End Slurm header ##########
 #=
 # load modules
@@ -14,5 +14,5 @@
 
 # export JULIA_DEPOT_PATH=$SCRATCH
 export ON_CLUSTER=1
-exec julia --color=no --threads=96 --startup-file=no "$LCDIR/LightCones/averaging_procedure/shared_krylov_sector.jl" 17 50 50 BS z $1
+exec julia --color=no --threads=96 --startup-file=no "$LCDIR/LightCones/averaging_procedure/shared_krylov_sector_mixed.jl" 11 50 50 BS z $1
 =#
