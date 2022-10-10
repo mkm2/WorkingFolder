9e08afb63581d1695c86f8b91633ad02434570e1
mkm2 <Maximilian.Muellenbach@googlemail.com>
2022-10-10 17:04:59 +0200

    well

--- averaging_procedure/test_comparison.jl
+++ averaging_procedure/test_comparison.jl
@@ -19,28 +19,36 @@ TableOfContents()
 
 # ╔═╡ 01b54b4d-47dc-4a57-9490-b691de6fd997
 begin
-		function disorder_mean(A,n_shots)
+		function disorder_mean(A::Array{Float64,4},n_shots)
 			return mean(A[:,:,1:n_shots,:];dims=3)[:,:,1,:]
 		end
 		
-		function disorder_std(A,n_shots)
+		function disorder_std(A::Array{Float64,4},n_shots)
 			return std(A[:,:,1:n_shots,:];dims=3)[:,:,1,:]
 		end
 
-		function disorder_mean(A,n_shots)
+		function disorder_mean(A::Array{Float64,3},n_shots)
 			return mean(A[:,:,1:n_shots];dims=3)[:,:,1]
 		end
 		
-		function disorder_std(A,n_shots)
+		function disorder_std(A::Array{Float64,3},n_shots)
 			return std(A[:,:,1:n_shots];dims=3)[:,:,1]
 		end
 
-		function state_mean(A,n_states)
+		function state_mean(A::Array{Float64,4},n_states)
 			return mean(A[:,:,:,1:n_states];dims=4)[:,:,:,1]
 		end
 
-		function state_std(A,n_states)
+		function state_std(A::Array{Float64,4},n_states)
 			return std(A[:,:,:,1:n_states];dims=4)[:,:,:,1]
+		end
+
+		function state_mean(A::Array{Float64,3},n_states)
+			return mean(A[:,:,1:n_states];dims=3)[:,:,1]
+		end
+
+		function state_std(A::Array{Float64,3},n_states)
+			return std(A[:,:,1:n_states];dims=3)[:,:,1]
 		end
 	
 		#function pos_mean(A)
@@ -54,7 +62,7 @@ end
 
 # ╔═╡ 264929c4-375e-4146-a61c-9ce0782433d9
 begin
-	N = 11
+	N = 13
 	path = pwd() *"/test/"
 	shots = 50
 	states = 50
@@ -67,9 +75,9 @@ md"# No disorder h=0"
 
 # ╔═╡ da94efac-baa7-4b7f-a049-554b026da248
 begin
-	f_df = "7153298_N11_BS.jld2"
-	f_sf = "7153301_N11_BS.jld2"
-	f_m = "7153309_N11_BS.jld2"
+	f_df = "7273734_N13_BS.jld2"
+	f_sf = "7273738_N13_BS.jld2"
+	f_m = "7273742_N13_BS.jld2"
 	
 	jobids_df = load(path*f_df,"jobid")
 	params_df = load(path*f_df,"params")
@@ -86,11 +94,26 @@ begin
 	size(data_m)
 end
 
-# ╔═╡ b6b888c7-b46f-49d0-8115-e755ec272027
-plot(trange,disorder_mean(data_m,2500))
+# ╔═╡ 4e5a645a-f930-4179-898a-7861584e5bb2
+n_m = 2500
 
-# ╔═╡ b83751a1-3ab9-41f3-b592-3c794762e8dd
-maximum(data_sf-data_df)
+# ╔═╡ 2291487f-b9a7-4d05-9cf8-ab326e285d68
+data_m
+
+# ╔═╡ 162feefd-1c8b-4d21-bf80-f662b47a319f
+
+
+# ╔═╡ c309b799-b9da-4b1c-90e7-b99ec5b0d516
+disorder_mean(data_sf,3)
+
+# ╔═╡ b6b888c7-b46f-49d0-8115-e755ec272027
+
+
+# ╔═╡ 0c32ce58-58b4-4c29-b9d8-8de92849cea0
+plot(mean(disorder_std(data_m,n_m);dims=1))
+
+# ╔═╡ aa5f26ed-c2f7-4191-8260-0865d5d610a0
+mean(disorder_std(data_m,n_m);dims=2)
 
 # ╔═╡ cc9e8551-6291-41fb-b0dd-36f4c2f2849d
 md"# Weak disorder h=4"
@@ -118,8 +141,13 @@ md"# Strong disorder h=12"
 # ╠═264929c4-375e-4146-a61c-9ce0782433d9
 # ╠═9c6e9d29-8041-494a-b30b-ff042cad3fe5
 # ╠═da94efac-baa7-4b7f-a049-554b026da248
+# ╠═4e5a645a-f930-4179-898a-7861584e5bb2
+# ╠═2291487f-b9a7-4d05-9cf8-ab326e285d68
+# ╠═162feefd-1c8b-4d21-bf80-f662b47a319f
+# ╠═c309b799-b9da-4b1c-90e7-b99ec5b0d516
 # ╠═b6b888c7-b46f-49d0-8115-e755ec272027
-# ╠═b83751a1-3ab9-41f3-b592-3c794762e8dd
+# ╠═0c32ce58-58b4-4c29-b9d8-8de92849cea0
+# ╠═aa5f26ed-c2f7-4191-8260-0865d5d610a0
 # ╠═cc9e8551-6291-41fb-b0dd-36f4c2f2849d
 # ╠═4357fec9-fec3-428e-99d4-210bf6150695
 # ╠═d407860a-5fc0-4928-bbb9-a86f79e29e86
