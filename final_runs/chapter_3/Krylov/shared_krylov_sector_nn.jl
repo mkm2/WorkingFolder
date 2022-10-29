7f9de7f90c6733025d49b8494c7b4d7f423ef8e0
Maximilian Müllenbach <maximilian.muellenbach@googlemail.com>
2022-10-29 22:15:12 +0200

    working changes

--- final_runs/chapter_3/Krylov/shared_krylov_sector_nn.jl
+++ final_runs/chapter_3/Krylov/shared_krylov_sector_nn.jl
@@ -76,7 +76,7 @@ logmsg("*"^10 * "Running simulation" * "*"^10)
 #Set up simulation parameters
 
 tmax = 0.5
-trange = 10. .^LinRange(-6,1,100)
+trange = 10. .^LinRange(-3,1,100)
 logmsg("trange = ",trange)
 
 i = div(N,2)+1
@@ -95,7 +95,7 @@ elseif OBSERVABLE == "z"
 end
 
 print("before H\n")
-H = symmetrize_operator(xxz(nearest_neighbourJ(N)),N,k)
+H = symmetrize_operator(xxz(nearest_neighbourJ_pbc(N)),N,k)
 print("after H\n")
 print(string("memory allocated: ",Base.summarysize(H)))
 print(string("memory of full H: ",Base.summarysize(xxz(N,6))))
--- final_runs/chapter_4/ED/Run_N13.jl
+++ final_runs/chapter_4/ED/Run_N13.jl
@@ -2,7 +2,7 @@
 # ########## Begin Slurm header ##########
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=16:00:00
+#SBATCH --time=20:00:00
 #SBATCH --mem=250gb
 #SBATCH --cpus-per-task=48
 #SBATCH --job-name=otoc_simulation_N13_ED
